14d94861482037959bee8326c1022b1b71ac4b4a
George Acquah <georgeacquah889@gmail.com>
2024-09-06 20:06:26 +0000

    updated testimonials

--- data/index.ts
+++ data/index.ts
@@ -107,25 +107,25 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with George was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. George's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, George is the ideal partner.",
     name: "Charles Asiedu",
     title: "Colleaque, UMaT",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with George was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. George's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, George is the ideal partner.",
     name: "Charles Asiedu",
     title: "Colleaque, UMaT",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with George was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. George's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, George is the ideal partner.",
     name: "Charles Asiedu",
     title: "Colleaque, UMaT",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with George was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. George's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, George is the ideal partner.",
     name: "Charles Asiedu",
     title: "Colleaque, UMaT",
   },
